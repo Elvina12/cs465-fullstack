77b19467af32a2404e8e50f33774454472c44c55
Elvina12 <medeiroselvina@gmail.com>
2021-10-15 18:57:12 -0400

    the updates to the database

--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,5 +1,8 @@
+const fs = require('fs');
+const room = JSON.parse(fs.readFileSync('./data/room.json', 'utf8'));
+
 const rooms = (req, res) => {
-    res.render('rooms', {title: 'Travelr Getaways'});
+    res.render('rooms', {title: 'Travelr Getaways', room});
 };
 
 module.exports = {
